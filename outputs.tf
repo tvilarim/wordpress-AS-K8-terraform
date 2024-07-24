4887958079ee7f837840866e1a1c8582e86f11d4
Vila <53236868+tvilarim@users.noreply.github.com>
2024-07-24 11:56:06 -0300

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "wordpress_url" {
+  value = "http://${local.minikube_ip}:${kubernetes_service.wordpress.spec.0.port.0.node_port}"
+}
+
+output "minikube_ip" {
+  value = local.minikube_ip
+}
+
+locals {
+  minikube_ip = trimspace(chomp(data.external.minikube_ip.result.output))
+}
+
+data "external" "minikube_ip" {
+  program = ["bash", "${path.module}/minikube_ip.sh"]
+}
